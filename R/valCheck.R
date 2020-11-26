e34ba4f517a97718253f853ff8a757a632aab2b7
Sophie Huebler <shuebler@ou.edu>
2020-11-26 16:53:58 -0600

    Setup for function 1

---
+++ R/valCheck.R
@@ -0,0 +1,11 @@
+#' Check the validity of a linear model
+#'
+#' @param lm a linear model
+#'
+#' @return command line statistics
+#' @export
+#'
+#' @examples
+valCheck<-function(lm){
+  print(lm$coefficients)
+}
